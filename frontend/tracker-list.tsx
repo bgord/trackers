d18c4d1f005ef908e9cef3db3e40a64c1aeff8d2
Bartosz Gordon <gordonbartosz@gmail.com>
2023-05-31 22:35:02 +0200

    step: use badge from bgord-design

--- frontend/tracker-list.tsx
+++ frontend/tracker-list.tsx
@@ -1,25 +1,28 @@
+import * as bg from "@bgord/frontend";
 import { h } from "preact";
 import { useQuery } from "react-query";
 
 import * as api from "./api";
 
 export function TrackerList() {
+  const t = bg.useTranslations();
+
   const trackerListQuery = useQuery("trackers", api.Tracker.list);
   const trackers = trackerListQuery.data ?? [];
 
   if (trackerListQuery.isLoading) {
-    return <div>Loading...</div>;
+    return <div data-mx="24">Loading...</div>;
   }
 
   if (trackerListQuery.isError) {
-    return <div>Error occurred</div>;
+    return <div data-mx="24">Error occurred</div>;
   }
 
   return (
-    <ul>
+    <ul data-display="flex" data-direction="column" data-gap="24" data-m="24">
       {trackers.map((tracker) => (
-        <li data-display="flex" data-gap="6">
-          <div>{tracker.kind}</div>
+        <li data-display="flex" data-gap="12">
+          <div class="c-badge">{t(`tracker.kind.enum.${tracker.kind}`)}</div>
           <strong>{tracker.name}</strong>
         </li>
       ))}
