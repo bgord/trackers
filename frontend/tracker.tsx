b3fe689916295fd8b29e2f792e332f7968a760f3
Bartosz Gordon <gordonbartosz@gmail.com>
2023-06-04 09:53:07 +0200

    step: setup details expansion

--- frontend/tracker.tsx
+++ frontend/tracker.tsx
@@ -10,24 +10,28 @@ export function Tracker(props: types.TrackerType) {
   const details = bg.useToggle();
 
   return (
-    <li data-display="flex" data-cross="center" data-gap="12">
-      <button
-        class="c-button"
-        type="button"
-        data-variant="with-icon"
-        title={
-          details.on ? t("tracker.details.hide") : t("tracker.details.show")
-        }
-        onClick={details.toggle}
-      >
-        {details.off && <Icons.NavArrowDown height="24" width="24" />}
-        {details.on && <Icons.NavArrowUp height="24" width="24" />}
-      </button>
+    <li data-display="flex" data-direction="column">
+      <div data-display="flex" data-cross="center" data-gap="12">
+        <button
+          class="c-button"
+          type="button"
+          data-variant="with-icon"
+          title={
+            details.on ? t("tracker.details.hide") : t("tracker.details.show")
+          }
+          onClick={details.toggle}
+        >
+          {details.off && <Icons.NavArrowDown height="24" width="24" />}
+          {details.on && <Icons.NavArrowUp height="24" width="24" />}
+        </button>
 
-      <div class="c-badge">{t(`tracker.kind.enum.${props.kind}`)}</div>
-      <div data-fs="14" data-color="gray-700">
-        {props.name}
+        <div class="c-badge">{t(`tracker.kind.enum.${props.kind}`)}</div>
+        <div data-fs="14" data-color="gray-700">
+          {props.name}
+        </div>
       </div>
+
+      {details.on && <div>details</div>}
     </li>
   );
 }
