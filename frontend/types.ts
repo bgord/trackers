d9911cc24d74402c2dbb1fd46b8ad19bb8319ef8
Bartosz Gordon <gordonbartosz@gmail.com>
2023-06-09 13:50:20 +0200

    step: add SettingsType

--- frontend/types.ts
+++ frontend/types.ts
@@ -4,6 +4,7 @@ import type { AsyncReturnType } from "@bgord/node";
 export type { EmailType } from "@bgord/node/dist/schema";
 
 import type { TrackerDatapointRepository } from "../repositories/tracker-datapoint-repository";
+import type { SettingsRepository } from "../repositories/settings-repository";
 
 export type { TrackerNameType } from "../value-objects/tracker-name";
 export type ToastType = bg.BaseToastType;
@@ -15,3 +16,5 @@ export type { TrackerType } from "../value-objects/tracker";
 export type TrackerDatapointType = AsyncReturnType<
   typeof TrackerDatapointRepository["list"]
 >[0];
+
+export type SettingsType = AsyncReturnType<typeof SettingsRepository["get"]>;
