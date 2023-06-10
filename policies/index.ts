2c4476c0b2da2c96b6534afea46a0321080e59d3
Bartosz Gordon <gordonbartosz@gmail.com>
2023-06-10 14:48:45 +0200

    step: add SettingsEmailIsConfigured policy

--- policies/index.ts
+++ policies/index.ts
@@ -6,3 +6,4 @@ export * from "./tracker-should-have-datapoints";
 export * from "./weekly-trackers-report-is-disabled";
 export * from "./weekly-trackers-report-is-enabled";
 export * from "./settings-email-should-change";
+export * from "./settings-email-is-configured";
---
+++ policies/settings-email-is-configured.ts
@@ -0,0 +1,24 @@
+import * as bg from "@bgord/node";
+
+import * as VO from "../value-objects";
+
+export class SettingsEmailIsConfiguredError extends Error {
+  constructor() {
+    super();
+    Object.setPrototypeOf(this, SettingsEmailIsConfiguredError.prototype);
+  }
+}
+
+type SettingsEmailIsConfiguredConfigType = {
+  email: VO.SettingsType["email"];
+};
+
+class SettingsEmailIsConfiguredFactory extends bg.Policy<SettingsEmailIsConfiguredConfigType> {
+  async fails(config: SettingsEmailIsConfiguredConfigType): Promise<boolean> {
+    return !config.email;
+  }
+
+  error = SettingsEmailIsConfiguredError;
+}
+
+export const SettingsEmailIsConfigured = new SettingsEmailIsConfiguredFactory();
