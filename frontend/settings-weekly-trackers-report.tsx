0e01570e072fb7ea468e7dffe7c50f4d1774b6e6
Bartosz Gordon <gordonbartosz@gmail.com>
2023-06-11 22:50:03 +0200

    feature: display weekly trackers report info

--- frontend/settings-weekly-trackers-report.tsx
+++ frontend/settings-weekly-trackers-report.tsx
@@ -1,10 +1,11 @@
 import * as bg from "@bgord/frontend";
-import { h } from "preact";
+import { h, Fragment } from "preact";
 import { useMutation, useQueryClient } from "react-query";
 
 import * as api from "./api";
 import * as hooks from "./hooks";
 import * as types from "./types";
+import * as UI from "./ui";
 
 export function SettingsWeeklyTrackersReport(props: types.SettingsType) {
   hooks.useLeavingPrompt();
@@ -35,46 +36,50 @@ export function SettingsWeeklyTrackersReport(props: types.SettingsType) {
     disableWeeklyTrackersReport.isLoading;
 
   return (
-    <div data-display="flex" data-cross="center" data-gap="24">
-      {props.isWeeklyTrackersReportEnabled && (
-        <div class="c-badge">
-          {t("settings.weekly_trackers_report.enabled")}
-        </div>
-      )}
+    <div data-display="flex" data-direction="column" data-gap="12">
+      <div data-display="flex" data-cross="center" data-gap="24">
+        {props.isWeeklyTrackersReportEnabled && (
+          <div class="c-badge">
+            {t("settings.weekly_trackers_report.enabled")}
+          </div>
+        )}
 
-      {!props.isWeeklyTrackersReportEnabled && (
-        <div class="c-badge">
-          {t("settings.weekly_trackers_report.disabled")}
-        </div>
-      )}
+        {!props.isWeeklyTrackersReportEnabled && (
+          <div class="c-badge">
+            {t("settings.weekly_trackers_report.disabled")}
+          </div>
+        )}
 
-      <div>{t("settings.weekly_trackers_report")}</div>
+        <div>{t("settings.weekly_trackers_report")}</div>
 
-      {props.isWeeklyTrackersReportEnabled && (
-        <button
-          class="c-button"
-          data-variant="primary"
-          data-ml="auto"
-          type="button"
-          onClick={() => disableWeeklyTrackersReport.mutate()}
-          disabled={canToggleWeeklyTrackersReport}
-        >
-          {t("settings.weekly_trackers_report.disable")}
-        </button>
-      )}
+        {props.isWeeklyTrackersReportEnabled && (
+          <button
+            class="c-button"
+            data-variant="primary"
+            data-ml="auto"
+            type="button"
+            onClick={() => disableWeeklyTrackersReport.mutate()}
+            disabled={canToggleWeeklyTrackersReport}
+          >
+            {t("settings.weekly_trackers_report.disable")}
+          </button>
+        )}
 
-      {!props.isWeeklyTrackersReportEnabled && (
-        <button
-          class="c-button"
-          data-variant="primary"
-          data-ml="auto"
-          type="button"
-          onClick={() => enableWeeklyTrackersReport.mutate()}
-          disabled={canToggleWeeklyTrackersReport}
-        >
-          {t("settings.weekly_trackers_report.enable")}
-        </button>
-      )}
+        {!props.isWeeklyTrackersReportEnabled && (
+          <button
+            class="c-button"
+            data-variant="primary"
+            data-ml="auto"
+            type="button"
+            onClick={() => enableWeeklyTrackersReport.mutate()}
+            disabled={canToggleWeeklyTrackersReport}
+          >
+            {t("settings.weekly_trackers_report.enable")}
+          </button>
+        )}
+      </div>
+
+      <UI.Info>{t("settings.weekly_trackers_report.info")}</UI.Info>
     </div>
   );
 }
