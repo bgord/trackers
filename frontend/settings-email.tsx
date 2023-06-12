c15e34cefa8b180aecc8cab20c71078c6fa6a6fa
Bartosz Gordon <gordonbartosz@gmail.com>
2023-06-12 20:09:33 +0200

    refactor: extract settings email change

--- frontend/settings-email.tsx
+++ frontend/settings-email.tsx
@@ -1,33 +1,16 @@
 import * as bg from "@bgord/frontend";
 import { h, Fragment } from "preact";
-import { useMutation, useQueryClient } from "react-query";
 import * as Icons from "iconoir-react";
 
-import * as api from "./api";
 import * as types from "./types";
 import * as UI from "./ui";
 
 import { SettingsEmailDelete } from "./settings-email-delete";
+import { SettingsEmailChange } from "./settings-email-change";
 
 export function SettingsEmail(props: types.SettingsType) {
   const t = bg.useTranslations();
-  const queryClient = useQueryClient();
   const details = bg.usePersistentToggle("settings-email");
-
-  const notify = bg.useToastTrigger();
-
-  const newEmailField = bg.useField<types.SettingsEmailType>(
-    "new-email-field",
-    "" as types.SettingsEmailType
-  );
-  const changeEmail = useMutation(api.Settings.emailChange, {
-    onSuccess: () => {
-      queryClient.invalidateQueries("settings");
-      notify({ message: "settings.email.change.success" });
-      newEmailField.clear();
-    },
-    onError: (error: bg.ServerError) => notify({ message: error.message }),
-  });
 
   return (
     <div data-display="flex" data-direction="column" data-gap="24">
@@ -85,56 +68,7 @@ export function SettingsEmail(props: types.SettingsType) {
             </div>
           )}
 
-          <form
-            data-display="flex"
-            data-cross="end"
-            data-wrap="nowrap"
-            data-width="100%"
-            data-gap="12"
-            onSubmit={(event) => {
-              event.preventDefault();
-              changeEmail.mutate({ email: newEmailField.value });
-            }}
-          >
-            <div
-              data-display="flex"
-              data-direction="column"
-              data-wrap="nowrap"
-              data-width="100%"
-            >
-              <label class="c-label" {...newEmailField.label.props}>
-                {t("settings.email.new.label")}
-              </label>
-
-              <input
-                class="c-input"
-                onChange={(event) =>
-                  newEmailField.set(event.currentTarget.value)
-                }
-                inputMode="email"
-                placeholder={t("settings.email.new.placeholder")}
-                required
-                data-width="100%"
-                value={newEmailField.value}
-                disabled={changeEmail.isLoading}
-                {...newEmailField.input.props}
-              />
-            </div>
-
-            <button
-              class="c-button"
-              data-variant="primary"
-              type="submit"
-              disabled={changeEmail.isLoading}
-            >
-              {t("settings.email.change")}
-            </button>
-
-            <UI.ClearButton
-              onClick={newEmailField.clear}
-              disabled={changeEmail.isLoading}
-            />
-          </form>
+          <SettingsEmailChange />
         </Fragment>
       )}
     </div>
