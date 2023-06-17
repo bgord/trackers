e33632d174900cd117c5d0516f04160346b915ba
Bartosz Gordon <gordonbartosz@gmail.com>
2023-06-17 09:52:57 +0000

    fix: tracker datapoints bar placeholder flickering

--- frontend/tracker-datapoints-bar-placeholder.tsx
+++ frontend/tracker-datapoints-bar-placeholder.tsx
@@ -7,16 +7,22 @@ export function TrackerDatapointsChartPlaceholder(props: { bars: number }) {
   const min = TRACKER_DATAPOINT_BOUND_LOWER;
   const max = TRACKER_DATAPOINT_BOUND_UPPER;
 
-  const bars = Array.from({ length: props.bars }).map(
+  const bars = Array.from({ length: props.bars - 1 }).map(
     () => Math.floor(Math.random() * (max - min + 1)) + min
   );
+
+  // Make sure at least one bar is at maximum height
+  bars.push(max);
 
   return (
     <ul
       data-display="flex"
       data-cross="end"
       data-gap="3"
-      style={{ height: `${max}px` }}
+      data-bwy="1"
+      data-bcy="gray-200"
+      data-py="3"
+      style={{ minHeight: `${max}px` }}
     >
       {bars.map((value, index) => (
         /* eslint-disable react/no-array-index-key */
