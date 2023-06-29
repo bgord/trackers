97973f477d69eb69c90d0db28c2787c871edf2f2
Bartosz Gordon <gordonbartosz@gmail.com>
2023-06-29 23:23:30 +0200

    refactor: api

--- frontend/api.ts
+++ frontend/api.ts
@@ -2,35 +2,21 @@ import * as bg from "@bgord/frontend";
 
 import * as types from "./types";
 
-export const _api: typeof fetch = (input, init) =>
-  fetch(input, {
-    mode: "same-origin",
-    headers: {
-      "Content-Type": "application/json",
-
-      "time-zone-offset": new Date().getTimezoneOffset().toString(),
-    },
-    redirect: "follow",
-    ...init,
-  })
-    .then(bg.ServerError.extract)
-    .catch(bg.ServerError.handle);
-
 export class Tracker {
   static async create(tracker: {
     name: types.TrackerType["name"];
     kind: types.TrackerKindEnum;
   }) {
-    return _api("/tracker", {
+    return bg.API("/tracker", {
       method: "POST",
       body: JSON.stringify(tracker),
     });
   }
 
   static async list(): Promise<types.TrackerType[]> {
-    return _api("/tracker", { method: "GET" }).then((response) =>
-      response.ok ? response.json() : []
-    );
+    return bg
+      .API("/tracker", { method: "GET" })
+      .then((response) => (response.ok ? response.json() : []));
   }
 
   static async sync(
@@ -38,28 +24,28 @@ export class Tracker {
       comment?: types.DatapointType["comment"];
     }
   ) {
-    return _api(`/tracker/${payload.id}/sync`, {
+    return bg.API(`/tracker/${payload.id}/sync`, {
       method: "POST",
       body: JSON.stringify({ value: payload.value, comment: payload.comment }),
     });
   }
 
   static async revert(payload: Pick<types.DatapointType, "id" | "trackerId">) {
-    return _api(`/tracker/${payload.trackerId}/revert/${payload.id}`, {
+    return bg.API(`/tracker/${payload.trackerId}/revert/${payload.id}`, {
       method: "DELETE",
     });
   }
 
   static async archive(payload: Pick<types.TrackerType, "id">) {
-    return _api(`/tracker/${payload.id}/archive`, { method: "POST" });
+    return bg.API(`/tracker/${payload.id}/archive`, { method: "POST" });
   }
 
   static async restore(payload: Pick<types.TrackerType, "id">) {
-    return _api(`/tracker/${payload.id}/restore`, { method: "POST" });
+    return bg.API(`/tracker/${payload.id}/restore`, { method: "POST" });
   }
 
   static async delete(payload: Pick<types.TrackerType, "id">) {
-    return _api(`/tracker/${payload.id}`, {
+    return bg.API(`/tracker/${payload.id}`, {
       method: "DELETE",
     });
   }
@@ -67,14 +53,14 @@ export class Tracker {
   static async export(
     payload: Pick<types.TrackerType, "id"> & { email: types.EmailType }
   ) {
-    return _api(`/tracker/${payload.id}/export`, {
+    return bg.API(`/tracker/${payload.id}/export`, {
       method: "POST",
       body: JSON.stringify({ email: payload.email }),
     });
   }
 
   static async changeName(payload: Pick<types.TrackerType, "id" | "name">) {
-    return _api(`/tracker/${payload.id}/name`, {
+    return bg.API(`/tracker/${payload.id}/name`, {
       method: "POST",
       body: JSON.stringify({ name: payload.name }),
     });
@@ -83,40 +69,40 @@ export class Tracker {
   static async getDatapoints(
     id: types.TrackerType["id"]
   ): Promise<types.DatapointType[]> {
-    return _api(`/tracker/${id}/datapoints`, { method: "GET" }).then(
-      (response) => (response.ok ? response.json() : [])
-    );
+    return bg
+      .API(`/tracker/${id}/datapoints`, { method: "GET" })
+      .then((response) => (response.ok ? response.json() : []));
   }
 }
 
 export class Settings {
   static async get(): Promise<types.SettingsType> {
-    return _api("/settings/data", { method: "GET" }).then((response) =>
-      response.json()
-    );
+    return bg
+      .API("/settings/data", { method: "GET" })
+      .then((response) => response.json());
   }
 
   static async weeklyTrackersReportEnable() {
-    return _api("/settings/weekly-trackers-report/enable", {
+    return bg.API("/settings/weekly-trackers-report/enable", {
       method: "POST",
     });
   }
 
   static async weeklyTrackersReportDisable() {
-    return _api("/settings/weekly-trackers-report/disable", {
+    return bg.API("/settings/weekly-trackers-report/disable", {
       method: "POST",
     });
   }
 
   static async emailChange(payload: { email: types.SettingsType["email"] }) {
-    return _api("/settings/email/change", {
+    return bg.API("/settings/email/change", {
       method: "POST",
       body: JSON.stringify({ email: payload.email }),
     });
   }
 
   static async emailDelete() {
-    return _api("/settings/email", { method: "DELETE" });
+    return bg.API("/settings/email", { method: "DELETE" });
   }
 }
 
@@ -124,7 +110,7 @@ export class Goal {
   static async create(
     goal: Pick<types.GoalType, "kind" | "target" | "relatedTrackerId">
   ) {
-    return _api("/goal", {
+    return bg.API("/goal", {
       method: "POST",
       body: JSON.stringify(goal),
     });
@@ -133,13 +119,15 @@ export class Goal {
   static async getForTracker(
     payload: Pick<types.GoalType, "relatedTrackerId">
   ): Promise<types.GoalType> {
-    return _api(`/tracker/${payload.relatedTrackerId}/goal`, {
-      method: "GET",
-    }).then((response) => (response.ok ? response.json() : undefined));
+    return bg
+      .API(`/tracker/${payload.relatedTrackerId}/goal`, {
+        method: "GET",
+      })
+      .then((response) => (response.ok ? response.json() : undefined));
   }
 
   static async delete(payload: Pick<types.GoalType, "id">) {
-    return _api(`/goal/${payload.id}`, {
+    return bg.API(`/goal/${payload.id}`, {
       method: "DELETE",
     });
   }
