c1ed7ce9e845b1dc9b438b65ac2fce20dfa7f9b3
Bartosz Gordon <gordonbartosz@gmail.com>
2023-07-08 19:20:23 +0200

    refactor: adjust tracker metadata for mobile

--- frontend/tracker-metadata.tsx
+++ frontend/tracker-metadata.tsx
@@ -11,24 +11,31 @@ export function TrackerMetadata(props: types.TrackerType) {
   const t = bg.useTranslations();
 
   return (
-    <div data-display="flex" data-gap="24" data-mt="6">
-      <UI.Info
-        title={bg.DateFormatter.datetime(props.createdAt.raw)}
-        data-color="gray-400"
-      >
-        <Icons.ClockOutline height="14" width="14" data-mr="6" />
-        {t("tracker.created_at", { value: props.createdAt.relative })}
-      </UI.Info>
+    <div data-display="flex" data-gap="12" data-mt="6">
+      <div data-display="flex" data-gap="12">
+        <UI.Info
+          title={bg.DateFormatter.datetime(props.createdAt.raw)}
+          data-color="gray-400"
+        >
+          <Icons.ClockOutline height="14" width="14" data-mr="6" />
+          {t("tracker.created_at", { value: props.createdAt.relative })}
+        </UI.Info>
 
-      <UI.Info
-        title={bg.DateFormatter.datetime(props.createdAt.raw)}
-        data-color="gray-400"
-      >
-        <Icons.ClockOutline height="14" width="14" data-mr="6" />
-        {t("tracker.updated_at", { value: props.updatedAt.relative })}
-      </UI.Info>
+        <UI.Info
+          title={bg.DateFormatter.datetime(props.createdAt.raw)}
+          data-color="gray-400"
+        >
+          <Icons.ClockOutline height="14" width="14" data-mr="6" />
+          {t("tracker.updated_at", { value: props.updatedAt.relative })}
+        </UI.Info>
+      </div>
 
-      <TrackerHistory data-ml="auto" {...props} />
+      <TrackerHistory
+        data-ml="auto"
+        data-md-ml="0"
+        data-md-mr="auto"
+        {...props}
+      />
     </div>
   );
 }
